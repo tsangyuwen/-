7f44bfb59ba5655baa2632df3e98f61155202ae6
tsang <B10423013@gemail.yuntech.edu.tw>
2018-08-25 23:40:46 +0800

    finish category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,21 +1,32 @@
 class CategoriesController < ApplicationController
+
   def woman
-    @categories = Category.where(who: "woman")
-    @products = Product.where(category_id: @categories).order(created_at: :desc)
+    basic_set("woman")
   end
 
   def man
-    @categories = Category.where(who: "man")
-    @products = Product.where(category_id: @categories).order(created_at: :desc)
+    basic_set("man")
   end
 
   def kid
-    @categories = Category.where(who: "kid")
-    @products = Product.where(category_id: @categories).order(created_at: :desc)
+    basic_set("kid")
   end
 
   def baby
-    @categories = Category.where(who: "baby")
+    basic_set("baby")
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    basic_set(@category.who)
+    @products = Product.where(category_id: @category).order(created_at: :desc)
+  end
+
+  private
+
+  def basic_set(who)
+    @categories = Category.where(who: who)
+    @where = Category.set_where(@categories)
     @products = Product.where(category_id: @categories).order(created_at: :desc)
   end
 
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
 class Category < ApplicationRecord
   has_many :products, dependent: :destroy
+
+  def self.set_where(categories)
+    where = []
+    categories.each do |c|
+      where << c.where
+    end
+    where = where.uniq
+  end
 end
