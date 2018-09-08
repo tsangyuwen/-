d6cabc8624ed04bc41e4edd013be8fe40b084561
tsang <B10423013@gemail.yuntech.edu.tw>
2018-09-09 00:49:15 +0800

    set deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 `ssh-add`
 
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+lock "3.11.0"
 
 set :application, "eggplant"
 set :repo_url, "git@github.com:tsangyuwen/eggplant.git"
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-#User.create(email: "eggplant@example.com", password: "ajion2596dhi", role: "admin", name: "root", confirmed_at: "2018-08-23")
-#User.create(email: "test@example.com", password: "123456", role: "general", name: "general", confirmed_at: "2018-08-23")
+User.create(email: "eggplant@example.com", password: "ajion2596dhi", role: "admin", name: "root", confirmed_at: "2018-08-23")
+User.create(email: "test@example.com", password: "123456", name: "test", confirmed_at: "2018-08-23")
 
 Category.destroy_all
 
