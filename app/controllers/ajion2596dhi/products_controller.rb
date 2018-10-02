30b48bf0a29fcf1fc4512ad55f72e9e984a4bc44
tsang <B10423013@gemail.yuntech.edu.tw>
2018-10-03 02:23:57 +0800

    fix cloud

--- app/controllers/ajion2596dhi/products_controller.rb
+++ app/controllers/ajion2596dhi/products_controller.rb
@@ -1,14 +1,5 @@
 class Ajion2596dhi::ProductsController < ApplicationController
   def index
-    require 'magic_cloud'
-
-    words = [
-      ['T-shirt', 50],
-      ['長袖', 40],
-      ['連帽', 30],
-    ]
-    @cloud = MagicCloud::Cloud.new(words, rotate: :free, scale: :log)
-    img = @cloud.draw(500, 500)
-    img.write("#{Rails.root}/app/assets/images/cloud.jpg")
+    @a = 200
   end
 end
