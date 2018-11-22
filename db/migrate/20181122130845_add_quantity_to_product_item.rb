aa7ffe3aa377a73dfbfacb0a5e1082c2c535257d
tsang <B10423013@gemail.yuntech.edu.tw>
2018-11-23 01:22:39 +0800

    fix migration

--- db/migrate/20181122130845_add_quantity_to_product_item.rb
+++
@@ -1,5 +0,0 @@
-class AddQuantityToProductItem < ActiveRecord::Migration[5.1]
-  def change
-    add_column :product_items, :stock, :int
-  end
-end
--- db/migrate/20181122142326_change_product_and_product_items.rb
+++ db/migrate/20181122142326_change_product_and_product_items.rb
@@ -10,15 +10,5 @@ class ChangeProductAndProductItems < ActiveRecord::Migration[5.1]
     remove_column :products, :intro
     remove_column :products, :price
     remove_column :products, :category_id
-    add_column :product_items, :name, :string
-    add_column :product_items, :main_image, :string
-    add_column :product_items, :intro, :string
-    add_column :product_items, :price, :int
-    add_column :product_items, :category_id, :int
-    remove_column :product_items, :size
-    remove_column :product_items, :color
-    remove_column :product_items, :image
-    remove_column :product_items, :stock
-    remove_column :product_items, :product_id
   end
 end
